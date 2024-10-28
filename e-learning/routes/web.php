7f33089ce836e5b9a9b95e675764b7cede519101
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-10-27 23:55:11 -0700

    e-learning - rutas

--- e-learning/routes/web.php
+++ e-learning/routes/web.php
@@ -3,5 +3,28 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "Bienvenido a la página principal";
 });
+
+// definiendo una segunda ruta
+
+Route::get('cursos', function () {
+    return "Bienvenido a la página cursos";
+});
+
+// definiendo una ruta con parametros
+
+Route::get('cursos/create', function () {
+    return "En esta pagina podras crear un curso";
+});
+
+Route::get('cursos/{curso}', function ($curso) {
+    return "Bienvenido al curso: " . $curso;
+});
+
+
+
+
+
+
