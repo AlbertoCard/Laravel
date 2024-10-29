9d088128df33f7160d20c32075e0b9aa1ddb25c8
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-10-28 22:46:52 -0700

    implementacion de grupo de rutas

--- e-learning/routes/web.php
+++ e-learning/routes/web.php
@@ -6,15 +6,14 @@ use App\Http\Controllers\CursoController;
 
 Route::get('/', HomeController::class);
 
-// definiendo una segunda ruta
 
-Route::get('cursos', [CursoController::class, 'index']);
+Route::controller(CursoController::class)->group(function (){
+    Route::get('cursos', 'index');
+    Route::get('cursos/create', 'create');
+    Route::get('cursos/{curso}', 'show');
+});
 
-// definiendo una ruta con parametros
 
-Route::get('cursos/create', [CursoController::class, 'create']);
-
-Route::get('cursos/{curso}',  [CursoController::class, 'show']);
 
 
 
