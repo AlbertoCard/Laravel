c79e2d1542a1e76349e7d7a2eff19c48892b20e1
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-11-15 21:08:44 -0700

    elearning - aplicacndo primeras vistas

--- e-learning/app/Http/Controllers/CursoController.php
+++ e-learning/app/Http/Controllers/CursoController.php
@@ -4,17 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use function Ramsey\Uuid\v1;
+
 class CursoController extends Controller
 {
     public function index(){
-        return "Bienvenido a la página cursos";
+        return view('cursos.index');
     }
 
     public function create(){
-        return "En esta pagina podras crear un curso";
+        return view('cursos.create');
     }
 
     public function show($curso){
-        return "Bienvenido al curso: " . $curso;
+        //se manda la variable a traves de un arreglo
+        //return view('cursos.show', ['curso' => $curso]);
+        return view('cursos.show', compact('curso'));
     }
 }
--- e-learning/routes/web.php
+++ e-learning/routes/web.php
@@ -8,9 +8,10 @@ Route::get('/', HomeController::class);
 
 
 Route::controller(CursoController::class)->group(function (){
+    // ruta : peticion( url, metodo)
     Route::get('cursos', 'index');
     Route::get('cursos/create', 'create');
-    Route::get('cursos/{curso}', 'show');
+    Route::get('cursos/{curso}', 'show'); 
 });
 
 
