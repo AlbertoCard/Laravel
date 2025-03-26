0c986603366a99adf38e740b230b4f499672850b
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2025-03-26 11:47:20 -0700

    loginFirebase - Rutas establecidas

--- loginFirebase/routes/web.php
+++ loginFirebase/routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'index'])->name('home');
 
-Route::get('/login', function () {
-    return view('login');
-});
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::get('/logados', [AuthController::class, 'logados'])->name('logados');
